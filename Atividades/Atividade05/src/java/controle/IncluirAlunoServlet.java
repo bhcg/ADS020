d9cb7ab60cfe31a9960b05e754d7458478f242bd
18214290016 <18214290016@OLAB04-D18.academico.iesb.br>
2020-03-11 21:50:02 -0300

    Atividade 05 - !!!

---
+++ Atividades/Atividade05/src/java/controle/IncluirAlunoServlet.java
@@ -0,0 +1,108 @@
+package controle;
+
+import static java.awt.SystemColor.text;
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import static sun.swing.SwingUtilities2.submit;
+import static sun.swing.SwingUtilities2.submit;
+
+/**
+ *
+ * @author 18214290016
+ */
+@WebServlet(name = "IncluirAlunoServlet", urlPatterns = {"/aluno/incluir"})
+public class IncluirAlunoServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet IncluirAlunoServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet IncluirAlunoServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Cadastro de Alunos</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Incluir Aluno</h1>");
+            out.println("<form action=\"/aluno/incluir\" method=\"post\">");
+            out.println("<div>");
+            out.println("<label>Matrícula:</label>");
+            out.println("<input type=\"text\" name=\"matricula\" size=\"15\">");
+            out.println("</div>");
+            out.println("<div>");
+            out.println("<label>Nome:</label>");
+            out.println("<input type=\"text\" name=\"nome\" size=\"30\">");
+            out.println("</div>");
+            out.println("<input type=\"submit\" value=\"Salvar\"/>");
+            out.println("<a href=\"/aluno/listar\">Desistir</a>");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- Atividades/Atividade05/src/java/controle/ListarAlunoServlet.java
+++ Atividades/Atividade05/src/java/controle/ListarAlunoServlet.java
@@ -1,5 +1,4 @@
-
-package controle;
+package Controle;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,105 +10,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelo.AlunoBO;
-import modelo.BO;
+import modelo.NegocioException;
 import modelo.entidades.Aluno;
 
 @WebServlet(name = "ListarAlunoServlet", urlPatterns = {"/aluno"})
 public class ListarAlunoServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ListarAlunoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ListarAlunoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<Aluno>  lista = new ArrayList<Aluno>();
-        AlunoBO bo = new AlunoBO();
-        try (PrintWriter out = response.getWriter()){
-        out.println("<!DOCTYPE html");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Cadastro de Alunos</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Listagem de Alunos</h1>");
-        out.println("<table>");
-        out.println("<tr>");
-        out.println("<th>Matricula</th>");
-        out.println("<th>Nome</th>");
-        out.println("<th>Ações</th>");
-        out.println("</tr>");
-        
-        for (Aluno aluno: lista){
-        out.println("<tr>");
-        out.println("<td>" + aluno.getMatricula() + "</td>");
-        out.println("<td>" + aluno.getMatricula() + "</td>");
-        out.println("<td><a href=''>Alterar</a></td>");
-        out.println("<td><a href=''>Excluir</a></td>");
-        out.println("</tr>");
+        response.setContentType("text/html;charset=UTF-8");
+      List<Aluno> lista = new ArrayList<Aluno>();
+      AlunoBO bo = new AlunoBO();
+       try {
+          lista = bo.listar();
+      } catch (NegocioException e) {
+          throw new ServletException("", e);
+      }
+      try (PrintWriter out = response.getWriter()) {
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Cadastro de Alunos</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Listagem de Alunos</h1>");
+            out.println("<table>");
+            out.println("<tr>");
+            out.println("<th>Matrícula</th>");
+            out.println("<th>Nome</th>");
+            out.println("<th>Ações</th>");
+            out.println("</tr>");
+            
+            for (Aluno aluno: lista){
+            out.println("<tr>");
+            out.println("<td>" + aluno.getMatricula() + "</td>");
+            out.println("<td>" + aluno.getNome() + "</td>");
+            out.println("<td><a href=''>Alterar</a></td>");
+             out.println("<td><a href=''>Excluir</a></td>");
+            out.println("</tr>");
+            }
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
         }
-        
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
-        
-        }
+      
+     
+      
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        throw new ServletException("Método não suportado");
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        return "Short deion";
+    }
 
 }
