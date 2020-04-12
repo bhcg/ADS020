aa183f60ae36cd1bf969ea5e49930e1cc6ea4444
brnhe <brnhe@BrunoH>
2020-04-12 20:49:43 -0300

    Criando Atividade10.

---
+++ Atividades/Atividade10/src/java/managedbeans/LoginMB.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author brnhe
+ */
+@ManagedBean
+@RequestScoped
+public class LoginMB {
+
+    private String usuario;
+    private String senha;
+    
+    public LoginMB() {
+        
+        
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    } 
+
+    
+
