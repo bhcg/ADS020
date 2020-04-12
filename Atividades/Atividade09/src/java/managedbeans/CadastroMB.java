49a96961f70b413fce7086dd083560968df8c95a
brnhe <brnhe@BrunoH>
2020-04-12 19:08:00 -0300

    Criando Atividade09

---
+++ Atividades/Atividade09/src/java/managedbeans/CadastroMB.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author brnhe
+ */
+@ManagedBean
+@RequestScoped
+public class CadastroMB {
+
+    public String salvar() {
+    return "lista.xhtml";
+    }
+    
+    public CadastroMB() {
+    }
+    
+}
