840c31ccb7911caf4ca1032bc0acfd17be347a35
brnhe <brnhe@BrunoH>
2020-04-21 20:05:45 -0300

    Criando Atividade10

--- Atividades/Atividade10/src/java/managebeans/LoginMB.java
+++ Atividades/Atividade10/src/java/managebeans/LoginMB.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package managedbeans;
+package managebeans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
